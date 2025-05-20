dab5cc4b2b5d24c87919d8929a683f06d796ea33
Alena Petrych <abelokonskaya.87@gmail.com>
2025-05-20 12:17:21 +0100

    update_challenge3_solutions

--- challenge3_compilation.js
+++ challenge3_compilation.js
@@ -2,10 +2,14 @@
 
 Scopes:
 Global {
-  
+  Line 16: let b - declared, TDZ
+  Line 24: var a - declared, initialized with undefined
+  Line: 26: var c - declared, initialized with undefined
 }
 
 Block {
+
+Line 21: let c - declared, TDZ
 
 }
 */
--- challenge3_execution.js
+++ challenge3_execution.js
@@ -1,6 +1,16 @@
 /* EXECUTION PHASE
 
-Line 1: 
-...
+Line 16: let b - initialized, assigned with value 5.
+
+Line 18: a = b. Variable a will be assigned value 5
+
+{
+Block scope. 
+Line 21. Let c - initialized, assigned value of a (5).
+}
+
+Line 24: var a - initialized, reassigned with value 1.
+
+Line 28: printed in consol a, b, c with value 1, 5, undefined.
 
 */
