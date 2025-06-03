7b051c6825904f0eb5817de2d3eb65f0addf308f
Alena Petrych <abelokonskaya.87@gmail.com>
2025-06-03 11:43:45 +0100

    feat: add challenge array helper method push element in the array if not exist

--- array_helper_methods_challenges.js
+++ array_helper_methods_challenges.js
@@ -295,3 +295,46 @@
 // console.log(elementIsIncluded({ title: "Banana" }, fruits)); // false
 
 // console.log(elementIsIncluded(25, primitiveTypesArray)); // true
+
+/**
+ * CHALLENGE 9
+ *
+ * Push element to array if not exists
+ */
+
+// const myNumbers = [123, 50, 27];
+
+/* Create a function "pushIfUnique" with two parameters "inputArray" and "newElement".
+
+If "inputArray" already contains "newElement" print "{newElement} is already in the array" to the console.
+
+Otherwise push "newElement" to the "inputArray". 
+
+NOTE: We assume that "inputArray" may contain only primitive variables types
+*/
+
+// const pushIfUnique = (inputArray, newElement) => {
+//   if (inputArray.includes(newElement)) {
+//     console.log(`${newElement} is already in the array`);
+//   } else {
+//     inputArray.push(newElement);
+//   }
+// };
+
+// // option with ternary operator
+// const pushIfUnique = (inputArray, newElement) =>
+//   inputArray.includes(newElement)
+//     ? console.log(newElement + " is already in the array")
+//     : inputArray.push(newElement);
+
+// pushIfUnique(myNumbers, 50); // "50 is already in the array"
+// console.log(myNumbers); // [123, 50, 27]
+
+// pushIfUnique(myNumbers, 80);
+// console.log(myNumbers); // [123, 50, 27, 80]
+
+// pushIfUnique(myNumbers, 80); // "80 is already in the array"
+// console.log(myNumbers); // [123, 50, 27, 80]
+
+// pushIfUnique(myNumbers, 77);
+// console.log(myNumbers); // [123, 50, 27, 80, 77]
