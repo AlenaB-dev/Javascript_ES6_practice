b89279468038381aa5ecbc89e7b979ade306145e
Alena Petrych <abelokonskaya.87@gmail.com>
2025-06-04 10:09:52 +0100

    feat: add reduce() array helper method challenge

--- array_helper_methods_challenges.js
+++ array_helper_methods_challenges.js
@@ -338,3 +338,41 @@ NOTE: We assume that "inputArray" may contain only primitive variables types
 
 // pushIfUnique(myNumbers, 77);
 // console.log(myNumbers); // [123, 50, 27, 80, 77]
+
+/**
+ * CHALLENGE 10
+ *
+ * REDUCE()
+ */
+
+const posts = [
+  {
+    title: "How to find JavaScript developer job?",
+    postId: 3421,
+    comments: 25,
+  },
+  {
+    title: "Is it hard to learn ES6?",
+    postId: 5216,
+    comments: 3,
+  },
+  {
+    title: "Should I learn React or Angular?",
+    postId: 8135,
+    comments: 12,
+  },
+];
+
+const minimalComentsQty = 10;
+
+/* Create a function "popularPostsIds" with two parameters "posts" and "minimalComentsQty".
+
+This function "popularPostsIds" should return an array of postIds of posts that have quantity of "comments" at least equal to "minimalComentsQty"
+*/
+const popularPostsIds = (posts, minimalComentsQty) =>
+  posts.reduce((postIds, post) => {
+    if (post.comments >= minimalComentsQty) postIds.push(post.postId);
+    return postIds;
+  }, []);
+
+console.log(popularPostsIds(posts, minimalComentsQty)); // [3421, 8135]
