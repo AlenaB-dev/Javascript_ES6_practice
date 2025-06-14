9835630b1a769124435ed7c3c7cf8f9a1a9cdcb0
Alena Petrych <abelokonskaya.87@gmail.com>
2025-06-14 11:14:03 +0100

    feat: add challenge delete object properties

--- array_object_destructuring_challenges.js
+++ array_object_destructuring_challenges.js
@@ -169,3 +169,44 @@ Original array of posts should not be mutated.
 // */
 
 // console.log(posts); // original array of posts
+
+/**
+ * CHALLENGE 5
+ *
+ * Delete object properties
+ *
+ */
+/*
+Use object destructuring with rest object properties to quickly remove specific properties from the object.
+
+Ensure that variables that will be used for destructuring of the deleted properties will not be accessible after the destructuring operation.
+*/
+
+// let person = {
+//   _id: "5ad8cefcc0971792dacb3f1f",
+//   index: 4,
+//   processed: false,
+//   cart: ["item1", "item2", "item3"],
+//   email: "slarsen@test.com",
+//   name: "Samanta Larsen",
+//   cartId: 435,
+// };
+
+// // чтобы переменные не были доступны после удаления, необходимо выражение ниже поместить в {} скобки, для ограничения области видимости удаляемых переменных.
+// {
+//   let _id, processed, cart;
+
+//   ({ _id, processed, cart, ...person } = person);
+// }
+
+// console.log(person);
+// /*
+// {
+//   index: 4,
+//   email: "slarsen@test.com",
+//   name: "Samanta Larsen",
+//   cartId: 435
+// }
+// */
+
+// console.log(_id); // [Error] ReferenceError: Can't find variable: _id
