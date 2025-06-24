09f52c2ac555684040ee82046ef6fa933e80df56
Alena Petrych <abelokonskaya.87@gmail.com>
2025-06-24 09:49:39 +0100

    feat: add practice. Compare performance in ES5 and ES6

--- babel_intro_practice.js
+++ babel_intro_practice.js
@@ -5,8 +5,40 @@
  * ES6 Rest operator
  */
 
-function fn(...a) {
-  console.log(a);
-}
+// function fn1(...a) {
+//   return a;
+// }
 
-fn(1, 2, 3); // (3) [1, 2, 3] // was return an array
+// function fn2() {
+//   for (
+//     var _len = arguments.length, a = Array(_len), _key = 0;
+//     _key < _len;
+//     _key++
+//   ) {
+//     a[_key] = arguments[_key];
+//   }
+
+//   return a;
+// }
+
+// // compare performance
+
+// const repeat = 1000000;
+// // ES 6
+// console.time("Function 1 with ES6 features");
+
+// for (let i = 0; i < repeat; i++) {
+//   fn1(1, 2, 3); // (3) [1, 2, 3] // was return an array
+// }
+
+// console.timeEnd("Function 1 with ES6 features"); // Function 1 with ES6 features: 0.02490234375 ms
+// // Function 1 with ES6 features: 1.6259765625 ms // with repeat const
+
+// // ES 5
+// console.time("Function 2 with ES5 features");
+
+// for (let i = 0; i < repeat; i++) {
+//   fn2(1, 2, 3); // (3) [1, 2, 3] // was return an array
+// }
+
+// console.timeEnd("Function 2 with ES5 features"); // Function 2 with ES5 features: 5.322021484375 ms
