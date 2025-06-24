1df1e7afe8ee49b303fa0c1a1b29fc935b6af630
Alena Petrych <abelokonskaya.87@gmail.com>
2025-06-24 10:32:53 +0100

    feat: add challenge. Compare performance ES5, ES6 in Destructuring.

--- babel_intro_challenges.js
+++ babel_intro_challenges.js
@@ -5,6 +5,36 @@
  *
  */
 
-let myArray = [1, 2, 3];
+// let myArray = [1, 2, 3];
 
-let a, b, c;
+// // ES6
+// function fn1(array) {
+//   let [a, b, c] = array;
+//   console.log(a, b, c); // 10000 - 1 2 3
+// }
+
+// // ES5
+// function fn2(array) {
+//   var a = array[0];
+//   var b = array[1];
+//   var c = array[2];
+// }
+
+// const repeat = 1;
+// // ES6
+// console.time("Function 1 with ES6 features");
+
+// for (let i = 0; i < repeat; i++) {
+//   fn1(myArray); //
+// }
+// console.timeEnd("Function 1 with ES6 features"); // Function 1 with ES6 features: 0.066162109375 ms
+
+// // ES5
+
+// console.time("Function 2 with ES5 features");
+
+// for (let i = 0; i < repeat; i++) {
+//   fn2(myArray);
+// }
+
+// console.timeEnd("Function 2 with ES5 features"); // Function 2 with ES5 features: 0.02001953125 ms
